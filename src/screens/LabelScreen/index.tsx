9d70af3fae8c7049d86ca062ff914f3fdf54123d
alexander <alexvoronin021083@gmail.com>
2024-07-11 21:18:03 +0300

    Feature: added ui for screen

--- src/screens/LabelScreen/index.tsx
+++ src/screens/LabelScreen/index.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, View} from 'react-native';
+import {colors, fonts} from '../../constants';
 
-export const ProfileScreen = () => {
-  return <Text>Profile Screen</Text>;
+export const LabelScreen = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text
+        style={{
+          fontFamily: fonts.albertSansSemiBold,
+          fontSize: 20,
+          color: colors.black,
+        }}>
+        Label Screen
+      </Text>
+    </View>
+  );
 };
--- src/screens/ProfileScreen/index.tsx
+++ src/screens/ProfileScreen/index.tsx
@@ -1,8 +1,18 @@
-import React from "react";
-import {Text} from "react-native";
+import React from 'react';
+import {Text, View} from 'react-native';
+import {colors, fonts} from '../../constants';
 
-export const LabelScreen = () => {
-    return (
-        <Text>Label Screen</Text>
-    )
-}
+export const ProfileScreen = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text
+        style={{
+          fontFamily: fonts.albertSansSemiBold,
+          fontSize: 20,
+          color: colors.black,
+        }}>
+        Profile Screen
+      </Text>
+    </View>
+  );
+};
